acade154b6f752bdf5ad5d85354218076224bc94
Lucas Lima de Souza <lucas.lima@vizir.com.br>
2015-02-13 16:55:30 -0200

    Sequindo em frente

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,10 +1,6 @@
 angular.module('manicure.controllers', [])
 
 .controller('AppCtrl', function($scope, $state) {
-  console.log("chamou");
-
-
-
 })
 
 .controller('ChatsCtrl', function($scope, Chats) {
@@ -28,7 +24,7 @@ angular.module('manicure.controllers', [])
 
 .controller('AccountCtrl', function($scope, $ionicSlideBoxDelegate, $state, Location) {
 
-  console.log($ionicSlideBoxDelegate.enableSlide(false));
+  console.log($ionicSlideBoxDelegate.enableSlide());
 
   $scope.states = Location.states();
   $scope.account = {
@@ -46,6 +42,31 @@ angular.module('manicure.controllers', [])
     }
   };
 
+  $scope.typeAccount = {
+    isManicure: false,
+    isClient: false,
+    valid: false
+  };
+
+  var validation = function(){
+
+    var message = "Preecha por favor";
+
+    if($scope.account.name === "") $scope.message.name = message;
+    if($scope.account.cellphone === "") $scope.message.cellphone = message;
+    if($scope.account.selectedState.id === 0 ) $scope.message.selectedState = message;
+    if($scope.account.selectedCity.id === 0 ) $scope.message.selectedCity = message;
+
+    if($scope.typeAccount.isManicure){
+
+      if($scope.account.time.feet === 0 ) $scope.message.time.feet = message;
+      if($scope.account.time.hands === 0) $scope.message.time.hands = message;
+      if($scope.account.price.feet === 0) $scope.message.price.feet = message;
+      if($scope.account.price.hands === 0) $scope.message.price.hands = message;
+    }
+
+  };
+
   $scope.updateCities = function(state){
     if(state.id != 0){
       $scope.cities = Location.cities(state.id);
@@ -54,6 +75,7 @@ angular.module('manicure.controllers', [])
   };
 
   $scope.save = function(){
+    console.log("vlaidation", $scope.account.name.$valid);
     console.log("-=-=saved=-=-", $scope.account);
   };
 
@@ -65,19 +87,15 @@ angular.module('manicure.controllers', [])
     $ionicSlideBoxDelegate.previous();
   };
 
-  $scope.typeAccount = {
-    isManicure: false
-  };
 
   $scope.defineAccount = function(response){
 
     if(response === 'yes'){
       $scope.typeAccount.isManicure = true;
     }else{
-      $scope.typeAccount.isManicure = false;
+      $scope.typeAccount.isClient = true;
     };
 
-    // $state.go("app.account");
   };
 
 })
